350ecba5fb92f562254d36474fceef477e57e8a3
Cancain <eriksson.tomas@gmail.com>
2019-09-21 23:50:27 +0200

    completed video 3

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,77 @@
-//use std::mem;
+// fn take(v: Vec<i32>) {
+//     println!("We took v: {}", v[10] + v[100]);
+// }
+
+// fn cop(a: i32, b: i32) {
+//     println!("{}", a + b);
+// }
+
+// fn re(v: Vec<i32>) -> Vec<i32> {
+//     println!("{}", v[120] + v[111]);
+//     v
+// }
+
+// fn borrow1(v: &Vec<i32>) {
+//     println!("{}", (*v)[10] + (*v)[12]);
+// }
+
+// fn borrow2(v: &Vec<i32>) {
+//     println!("{}", v[10] + v[11]);
+// }
 
 fn main() {
-    // let t = (1, 'a', false);
-    // let f = (2, t);
-    // let ff = (2, (1, 'a', false)); //same thing as above
-    // println!("{} {} {}", t.0, t.1, t.2);
-    // println!("{:#?}", ff); 
-    // let t = (1,2,3,4,5,6,7,8,9,10,11,12,13,14);
+    // let x = 1;
+    // let y = x;
 
-    // println!("{:?}", t);
-    
-    // let xs: [i32; 5] = [4,5,6,7,8];
-    // println!("{} {} {}", xs[0], xs.len(), mem::size_of_val(&xs))
-    // let ys = &xs[2..4];
-    // println!("{} {}", ys[0], ys[1]);
+    // {
+    //     let a = 10;
+    // }
 
-    // println!("{:?} {:?}", ys, xs);
-    
-    // let s = "String".to_string();
-    // let ss = String::from("String");
+    // x + a;
+    // let s = String::from("String");
+    // let y = &s;
 
-    // let slice = &ss[2..4];
-    // println!("{}", slice);
+    // println!("{}", s);
 
-    let h = String::from("Hello, ");
-    let w = String::from("World!");
-    let s = h + &w;
-    println!("{}", s);
+    // let mut v = Vec::new();
+
+    // for i in 1..1000 {
+    //     v.push(i);
+    // }
+    // take(v);
+
+    // println!("Finished");
+
+    // let a = 32;
+    // let b = 45;
+
+    // cop(a, b);
+
+    // println!("We have a: {} and b: {}", a, b);
+
+    // let mut v = Vec::new();
+    // for i in 1..1000 {
+    //     v.push(i);
+    // }
+
+    // v = re(v);
+
+    // println!("Still own v: {} {}", v[0], v[1]);
+
+    // borrow1(&v);
+    // println!("Still own v: {} {}", v[0], v[1]);
+
+    // borrow2(&v);
+    // println!("Still own v: {} {}", v[0], v[1]);
+
+    let v = vec![4, 5, 6, 4, 34, 5, 6, 4, 3, 3, 5, 5, 3, 23, 1];
+
+    for &i in &v {
+        let r = count(&v, i);
+        println!("{} is repeated {} times", i, r);
+    }
+}
+
+fn count(v: &Vec<i32>, val: i32) -> usize {
+    v.into_iter().filter(|&&x| x == val).count()
 }
